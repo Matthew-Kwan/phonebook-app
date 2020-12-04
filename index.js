c1b19a5615f7c2ca95f8710191ba2503066bf459
Matthew-Kwan <matthew.k913@gmail.com>
2020-12-03 23:17:13 -0500

    Fixed bug with error message in the frontend

--- index.js
+++ index.js
@@ -120,11 +120,7 @@ app.put('/api/persons/:id', (request, response, next) => {
     number: body.number,
   }
 
-  Person.findById(request.params.id).then(person => {
-    console.log(person)
-  })
-  
-  Person.findByIdAndUpdate(request.params.id, { name: request.body.name, number: request.body.number}, {new: true, runValidators: true,})
+  Person.findByIdAndUpdate(request.params.id, person, {new: true, runValidators: true, context: 'query'})
     .then(updatedPerson => {
       response.json(updatedPerson) 
     })
